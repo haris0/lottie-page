7ce1215856a4df76650fe5f1d0521ca728fdf74d
Haris <hari@pijar.com>
2025-11-04 18:21:40 +0700

    implement home page

---
+++ src/app/LottieAnimation.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import Lottie from "lottie-react";
+import animationData from "../../public/JB2G_Lottie.json";
+
+export default function LottieAnimation() {
+  return <Lottie animationData={animationData} />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
 import Image from "next/image";
+import LottieAnimation from "./LottieAnimation";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        Test
-      </main>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div style={{ width: 46 }} />
+        <Image src="/logo.png" alt="Logo" width={123} height={29} />
+        <button className={styles.refresh}>
+          <Image src="/refresh.png" alt="Refresh" width={20} height={20} />
+        </button>
+      </header>
+      <section className={styles.section}>
+        <div className={styles.animation_container}>
+          <LottieAnimation />
+        </div>
+        <div className={styles.section_texts}>
+          <span>WA businesses feel confident about future growth</span>
+          <span>AI cant replace creativity</span>
+          <span>Sales measure true success</span>
+          <span>Human connection drives WA business</span>
+          <span>
+            The primary barrier to digital transformation is financial investment
+          </span>
+        </div>
+      </section>
+      <footer className={styles.footer}>
+        <h2 className={styles.footer_text}>
+          Compare your thoughts on{" "}
+          <span className={styles.footer_text_highlight}>technology</span> with
+          current industry opinions.
+        </h2>
+        <button className={styles.footer_button}>Get a reality check</button>
+      </footer>
     </div>
   );
 }
