02f79aba0db07025132ad410d709b40e47f12e0c
Haris <hari@pijar.com>
2025-11-05 09:23:24 +0700

    minor improvement

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@
 import Image from "next/image";
 import styles from "./Header.module.css";
 import { useRouter } from 'next/navigation';
+import Link from "next/link";
 
 interface HeaderProps {
   showBackButton?: boolean;
@@ -27,7 +28,9 @@ const Header = ({ showBackButton }: HeaderProps) => {
           <Image src="/back.png" alt="Back" width={20} height={20} />
         </button>
       ) : (<div style={{ minWidth: 46 }} />)}
-      <Image src="/logo.png" alt="Logo" width={123} height={29} />
+      <Link href="/">
+        <Image src="/logo.png" alt="Logo" width={123} height={29} />
+      </Link>
       <button className={styles.button} onClick={handleRefresh}>
         <Image src="/refresh.png" alt="Refresh" width={20} height={20} />
       </button>
